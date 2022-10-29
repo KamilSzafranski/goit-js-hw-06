5a15521f93b01761fe4e365af8afc537f6f2637b
Kamil Szafrański <107311803+KamilSzafranski@users.noreply.github.com>
2022-10-29 10:02:33 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -3,9 +3,11 @@ const input = document.querySelector("#validation-input");
 
 input.addEventListener("blur", (event) => {
   const inputLenght = event.currentTarget.value.split("").length;
-  if (inputLenght < 6) {
-    return input.classList.add("invalid");
+  if (inputLenght === 6 ) {
+    input.classList.add("valid");
+    input.classList.remove("invalid")
+    return 
   }
 
-  return input.classList.add("valid");
+  return input.classList.add("invalid");
 });
